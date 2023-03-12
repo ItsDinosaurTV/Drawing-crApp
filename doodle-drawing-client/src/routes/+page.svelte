55f4e7990fe929d00f7be0cbff518e5fac00d35e
Cameron Tyner <43007646+CameronTyner@users.noreply.github.com>
2023-03-12 04:14:44 -0400

    Add drawing tools, layout

--- doodle-drawing-client/src/routes/+page.svelte
+++ doodle-drawing-client/src/routes/+page.svelte
@@ -6,39 +6,37 @@
 	let showCanvas = false;
 </script>
 
-<form>
-	{#if showCanvas}
-		<DrawCanvas on:back={(_) => (showCanvas = false)} />
-	{:else}
-		<button
-			class="border border-dashed rounded-lg h-52 mb-2 w-full flex justify-center items-center"
-			on:click={(_) => (showCanvas = true)}
-		>
-			<div>Click here to draw</div>
-		</button>
+{#if showCanvas}
+	<DrawCanvas on:back={() => (showCanvas = false)} />
+{:else}
+	<button
+		class="border border-dashed rounded-lg h-52 mb-2 w-full flex justify-center items-center"
+		on:click={() => (showCanvas = true)}
+	>
+		<div>Click here to draw</div>
+	</button>
 
-		<div class="max-w-sm mx-auto">
-			<input
-				bind:value={character.name}
-				type="text"
-				placeholder="Character Name"
-				class="input input-bordered border-gray-400 w-full text-xl"
-				maxlength="32"
-			/>
-		</div>
-
-		<hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700" />
-
-		<textarea
-			bind:value={character.description}
-			class="textarea textarea-bordered border-gray-400 w-full max-h-80 leading-relaxed"
-			placeholder="Describe the character's personality"
-			maxlength="1024"
-			rows="5"
+	<div class="max-w-sm mx-auto">
+		<input
+			bind:value={character.name}
+			type="text"
+			placeholder="Character Name"
+			class="input input-bordered border-gray-400 w-full text-xl"
+			maxlength="32"
 		/>
+	</div>
 
-		<div class="text-center">
-			<button class="btn mx-auto" type="submit">Submit</button>
-		</div>
-	{/if}
-</form>
+	<hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700" />
+
+	<textarea
+		bind:value={character.description}
+		class="textarea textarea-bordered border-gray-400 w-full max-h-80 leading-relaxed"
+		placeholder="Describe the character's personality"
+		maxlength="1024"
+		rows="5"
+	/>
+
+	<div class="text-center">
+		<button class="btn mx-auto" type="submit">Submit</button>
+	</div>
+{/if}
