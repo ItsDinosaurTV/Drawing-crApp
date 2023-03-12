abbc6fe86831e50540f5881622d9402211b63fa7
Cameron Tyner <43007646+CameronTyner@users.noreply.github.com>
2023-03-11 22:57:41 -0500

    Add initial character creation layout

--- doodle-drawing-client/src/routes/+page.svelte
+++ doodle-drawing-client/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form>
+	<div class="border border-dashed rounded-lg h-52 mb-2 w-full flex justify-center items-center">
+		<div>Click here to draw</div>
+	</div>
+	<div class="max-w-sm mx-auto">
+		<input
+			type="text"
+			placeholder="Character Name"
+			class="input input-bordered border-gray-400 w-full text-xl"
+			maxlength="32"
+		/>
+	</div>
+	<hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700" />
+	<textarea
+		class="textarea textarea-bordered border-gray-400 w-full max-h-80 leading-relaxed"
+		placeholder="Describe the character's personality"
+		maxlength="1024"
+		rows="5"
+	/>
+	<div class="text-center">
+		<button class="btn mx-auto" type="submit">Submit</button>
+	</div>
+</form>
